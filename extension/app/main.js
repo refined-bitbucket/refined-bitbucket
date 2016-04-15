4258d811183420e06c5f639827238626bdc705de
andremw <mazoni.andre@gmail.com>
2016-04-15 18:05:32 -0300

    Refactors the code to a better organization
    Used promises in the waitForRender and changed it to receive a selector
    as parameter

--- extension/app/main.js
+++ extension/app/main.js
@@ -1,9 +1,9 @@
 require({
-        baseUrl: chrome.extension.getURL("/"),
-        paths: { jquery: "//ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min" }
-    }, ["app/highlighter", "app/doubleClickWordSelection"],
-    function(highlighter, doubleClickWordSelection) {
-        highlighter.run();
+    baseUrl: chrome.extension.getURL("/"),
+    paths: {jquery: "//ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min"}
+}, ["app/highlighter", "app/doubleClickWordSelection"],
+    (highlighter, doubleClickWordSelection) => {
+        highlighter.init();
         doubleClickWordSelection.run();
     }
 );
