9e7c410debeb0ca01b494433f104daf64f84095d
andremw <mazoni.andre@gmail.com>
2016-04-17 21:33:50 -0300

    Fixes minor problems
    * Fixes doubleClickWordSelection to work in the commit screen as well

--- extension/app/highlighter.js
+++ extension/app/highlighter.js
@@ -8,6 +8,7 @@ define(['../var/document', '../var/languages'], (document, languages) => {
     return {
         init() {
             insertStyles();
+            bindRehighlightHandler();
             waitForRender('.diff-container').then(() => {
                 highlightCode();
             });
@@ -20,14 +21,20 @@ define(['../var/document', '../var/languages'], (document, languages) => {
      * the styles are not being injected into the page.
      */
     function insertStyles() {
-        const head = document.getElementsByTagName('head')[0];
-        const style = document.createElement('style');
+        let head = document.getElementsByTagName('head')[0];
+        let lastHeadElement = head.lastChild;
+        let style = document.createElement('style');
+        const styleArray = [];
         style.type = 'text/css';
         // Prism css
-        style.innerHTML = '.token.comment,.token.prolog,.token.doctype,.token.cdata{color: slategray}.token.punctuation{color: #999}.namespace{opacity: .7}.token.property,.token.tag,.token.boolean,.token.number,.token.constant,.token.symbol,.token.deleted{color: #905}.token.selector,.token.attr-name,.token.string,.token.char,.token.builtin,.token.inserted{color: #690}.token.operator,.token.entity,.token.url,.language-css .token.string,.style .token.string{color: #a67f59;background: hsla(0, 0%, 100%, .5)}.token.atrule,.token.attr-value,.token.keyword{color: #07a}.token.function{color: #DD4A68}.token.regex,.token.important,.token.variable{color: #e90}.token.important,.token.bold{font-weight: bold}.token.italic{font-style: italic}.token.entity{cursor: help}';
+        styleArray.push('.token.comment,.token.prolog,.token.doctype,.token.cdata{color: slategray}.token.punctuation{color: #999}.namespace{opacity: .7}.token.property,.token.tag,.token.boolean,.token.number,.token.constant,.token.symbol,.token.deleted{color: #905}.token.selector,.token.attr-name,.token.string,.token.char,.token.builtin,.token.inserted{color: #690}.token.operator,.token.entity,.token.url,.language-css .token.string,.style .token.string{color: #a67f59;background: hsla(0, 0%, 100%, .5)}.token.atrule,.token.attr-value,.token.keyword{color: #07a}.token.function{color: #DD4A68}.token.regex,.token.important,.token.variable{color: #e90}.token.important,.token.bold{font-weight: bold}.token.italic{font-style: italic}.token.entity{cursor: help}');
         // Custom css to fix some layout problems because of the insertion of <code> element
-        style.innerHTML += 'pre>code{border-radius:initial;display:initial;line-height:initial;margin-left:initial;overflow-y:initial;padding:initial}code,tt{background:initial;border:initial}.refract-container .deletion pre.source {background-color: #fff1f2 !important;} .refract-container .addition pre.source { background-color: #e8ffe8;}';
-        head.appendChild(style);
+        styleArray.push('pre>code{border-radius:initial;display:initial;line-height:initial;margin-left:initial;overflow-y:initial;padding:initial}code,tt{background:initial;border:initial}.refract-container .deletion pre.source {background-color: #fff1f2 !important;} .refract-container .addition pre.source { background-color: #e8ffe8;}');
+        style.innerHTML = styleArray.join('');
+        head.insertBefore(style, lastHeadElement);
+        head = null;
+        lastHeadElement = null;
+        style = null;
     }
 
     /**
@@ -50,7 +57,6 @@ define(['../var/document', '../var/languages'], (document, languages) => {
     }
 
     function highlightCode() {
-        bindRehighlightHandler();
         prepareLanguageClasses();
         addCodeTagToElements('.source');
         Prism.highlightAll();
@@ -62,6 +68,10 @@ define(['../var/document', '../var/languages'], (document, languages) => {
      */
     function bindRehighlightHandler() {
         const menuDiff = document.getElementById('pr-menu-diff');
+        if (!menuDiff) {
+            return;
+        }
+
         menuDiff.addEventListener('click', () => {
             waitForRender('.diff-container').then(() => {
                 highlightCode();
