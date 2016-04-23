6cf29e0447001faf0501e3cf404471e54edfad22
andremw <mazoni.andre@gmail.com>
2016-04-23 09:41:47 -0300

    Adds initial tests

---
+++ test/codeStructurer.spec.js
@@ -0,0 +1,9 @@
+const test = require('tape');
+const structurer = require('../extension/lib/code-structurer');
+
+test('<code> structurer', (t) => {
+    t.test('create tag structure that Prism needs', (t) => {
+  
+    });
+    t.end();
+});
---
+++ test/test.js
@@ -0,0 +1,6 @@
+var test = require('tape');
+
+test('a test', (t) => {
+    t.ok(true);
+    t.end();
+});
