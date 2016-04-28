99b0a5556d71b3ffabca51526e04b9e48d4ec8ce
andremw <mazoni.andre@gmail.com>
2016-04-28 20:46:37 -0300

    Clears everything to start over again but testing from the beginning.

--- test/codeStructurer.spec.js
+++
@@ -1,9 +0,0 @@
-const test = require('tape');
-const structurer = require('../extension/lib/code-structurer');
-
-test('<code> structurer', (t) => {
-    t.test('create tag structure that Prism needs', (t) => {
-  
-    });
-    t.end();
-});
--- test/test.js
+++
@@ -1,6 +0,0 @@
-var test = require('tape');
-
-test('a test', (t) => {
-    t.ok(true);
-    t.end();
-});
