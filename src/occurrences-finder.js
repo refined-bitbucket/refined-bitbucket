2bc0e171e26fa8c3b5d738a30762a3b5c3434511
Andre Mazoni <mazoni.andre@gmail.com>
2016-06-30 21:34:41 -0300

    Adds initial version of occurrencesHighlighter

---
+++ src/occurrences-finder.js
@@ -0,0 +1,37 @@
+window.occurrencesHighlighter = (function () {
+    function highlight() {
+        const selection = window.getSelection();
+        const selectedText = selection.toString();
+        const codeContainer = findCodeContainer(selection);
+        const codeLines = Array.from(codeContainer.querySelectorAll('code'));
+
+        codeLines.forEach(line => {
+            line.innerHTML = line.innerHTML.replace(new RegExp(`\\b${selectedText}\\b`, 'g'), `<span class='occ-highlight'>$&</span>`);
+        });
+    }
+
+    function removeHighlight() {
+        const highlighted = Array.from(document.getElementsByClassName('occ-highlight'));
+        highlighted.forEach(el => {
+            el.outerHTML = el.textContent;
+        });
+    }
+
+    function findCodeContainer(selection) {
+        let container = null;
+        let node = selection.getRangeAt(0).startContainer.parentNode;
+        while (container === null && node !== null) {
+            if (node.className === 'refract-content-container') {
+                container = node;
+                break;
+            }
+            node = node.parentNode;
+        }
+        return container;
+    }
+
+    return {
+        highlight,
+        removeHighlight
+    };
+})();
