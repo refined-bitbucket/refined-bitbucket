679756a55edc01fe9af03569a651ef6f16553ed2
Gregory McQuillan <gmcquillan@gmail.com>
2016-09-15 20:41:05 -0700

    (29-support-keyboard-binds) added comments
    - Added some doc comments
    - Added some code that may allow us to add customizable keymaps in the
      future to override the default keymap.
    - Switched some functions to use ES6 arrow function syntax, since I saw
      it elsewhere in the code.

--- src/keymap/keymap.js
+++ src/keymap/keymap.js
@@ -1,13 +1,16 @@
-/**
- * Adds useful keymappings to pull requests on bitbucket.org
- */
-
 const waitForRender = require('../wait-for-render');
 
+/**
+ * Adds useful keymappings to pull requests on bitbucket.org
+ * @module keymap
+ */
 var PrKeyMap = (function($) {
     'use strict';
 
-    var keymap = {
+    /**
+     * The default keymap for key binds a.k.a. shortcut keys.
+     */
+    const default_keymap = {
         tab_overview: '1',
         tab_commits: '2',
         tab_activity: '3',
@@ -16,12 +19,14 @@ var PrKeyMap = (function($) {
         scroll_previous_comment: 'P',
         scroll_page_top: 'g g',
         scroll_page_bottom: 'G'
-    }
+    };
 
-    var ids = {
-        'overview': '#pr-menu-diff',
-        'commits': '#pr-menu-commits',
-        'activity': '#pr-menu-activity'
+    var keymap = {};
+
+    const ids = {
+        overview: '#pr-menu-diff',
+        commits: '#pr-menu-commits',
+        activity: '#pr-menu-activity'
     };
 
     var self = {};
@@ -32,6 +37,9 @@ var PrKeyMap = (function($) {
 
     self.currentComment = 0;
 
+    /**
+     * Switches to a tab, if there is a selector available for that tab.
+     */
     self.switchTo = function(tabName) {
         if (tabName in ids) {
             const element = document.querySelector(ids[tabName]);
@@ -39,18 +47,38 @@ var PrKeyMap = (function($) {
         }
     };
 
+    /**
+     * Sets focus to a particular comment.
+     *
+     * 'j' and 'k' by default will go down and up (respectively) through iterable
+     * elements on the page (file list, comments, patches).
+     * This provides compatibility with BitBucket's built in key binds
+     * so that we don't break the existing functionality of the 'j' and 'k' keys.
+     *
+     */
     self.focusComment = function(comment) {
         $(self.iterableItemSelector).removeClass('focused');
         $(comment).addClass('focused');
         comment.scrollIntoView();
     };
 
+    /**
+     * Initializes array of comments elements to cycle through using
+     * {@link #scrollToNextComment} or {@link #scrollToPreviousComment}.
+     */
     self.initComments = function() {
         if (self.comments.length === 0) {
             self.comments = document.querySelectorAll(self.commentSelector);
         }
     };
 
+    /**
+     * Scrolls the browser window to the previous comment on the PR diff.
+     *
+     * If scrolling to the next comment from the last comment, this will loop back
+     * to the top-most (first) comment on the page.
+     *
+     */
     self.scrollToNextComment = function() {
         self.initComments();
 
@@ -64,6 +92,12 @@ var PrKeyMap = (function($) {
         }
     };
 
+    /**
+     * Scrolls the browser window to the previous comment on the PR diff.
+     *
+     * If scrolling to previous comment from the top commit, this will loop back to the
+     * bottom-most (last) comment on the page.
+     */
     self.scrollToPreviousComment = function() {
         self.initComments();
 
@@ -76,47 +110,59 @@ var PrKeyMap = (function($) {
         }
     };
 
-    self.init = function(keyboard) {
+    /**
+     * Initializes keybinds.
+     *
+     * @param {Object} keyboard the keyboard library to use to bind keys (usually Mousetrap).
+     * @param {Object} user_keymap the user-defined keymap to override default keybindings.
+     */
+    self.init = function(keyboard, userKeymap) {
 
-        console.log('init');
+        Object.assign(keymap, default_keymap);
+        if (userKeymap) {
+            //if provided, copy a user-preferred keymap to the main keymap.
+            Object.assign(keymap, userKeymap);
+        }
+
         keyboard.reset();
         self.comments = document.querySelectorAll(self.commentSelector);
 
-        keyboard.bind(keymap.tab_overview, function(event) {
+        keyboard.bind(keymap.tab_overview, (event) => {
             event.preventDefault();
             self.switchTo('overview');
+            waitForRender('.bb-patch').then(() => {
         });
 
-        keyboard.bind(keymap.tab_commits, function(event) {
+        keyboard.bind(keymap.tab_commits, (event) => {
             event.preventDefault();
             self.switchTo('commits');
         });
 
-        keyboard.bind(keymap.tab_activity, function(event) {
+        keyboard.bind(keymap.tab_activity, (event) => {
             event.preventDefault();
             self.switchTo('activity');
         });
 
         waitForRender('.bb-patch').then(() => {
             //only bind next and previous comments when the diff patches have finished loading
-            keyboard.bind(keymap.scroll_next_comment, function(event) {
+            keyboard.bind(keymap.scroll_next_comment, (event) => {
                 event.preventDefault();
                 self.scrollToNextComment();
             });
 
-            keyboard.bind(keymap.scroll_previous_comment, function(event) {
+            keyboard.bind(keymap.scroll_previous_comment, (event) => {
                 event.preventDefault();
                 self.scrollToPreviousComment();
             });
         });
 
-        keyboard.bind(keymap.scroll_page_top, function(event) {
+        keyboard.bind(keymap.scroll_page_top, (event) => {
             //gg to scroll to top
             event.preventDefault();
             window.scrollTo(0, 0);
         });
 
-        keyboard.bind(keymap.scroll_page_bottom, function(event) {
+        keyboard.bind(keymap.scroll_page_bottom, (event) => {
             //scroll to bottom
             window.scrollTo(0, document.body.scrollHeight);
         });
