1ff44e59c2ce48ffa26700e031c7079db044b487
Ronald Rey <reyronald@gmail.com>
2017-10-06 08:35:10 -0400

    feat: allow adding Bitbucket server domain
    Adds an input to the Options window to allow configuring
    the URL of a self-hosted Bitbucket server.
    
    Closes #39

--- extension/options.js
+++ extension/options.js
@@ -6,22 +6,87 @@
     const highlightSyntaxElement = document.getElementById('syntax-highlight');
     const highlightOcurrencesElement = document.getElementById('highlight-ocurrences');
     const approvalsElement = document.getElementById('approvals');
+    const bitbucketServerDomainElement = document.getElementById('bitbucket-server-domain');
     const statusElement = document.getElementById('status');
 
-    function saveOptions() {
+    async function saveOptions() {
+        if (!bitbucketServerDomainElement.value) {
+            storeOptions();
+            return;
+        } else {
+            if (!chrome.permissions) {
+                setFailedStatus('Your browser doesn\'t support the `permissions` API to authorize a custom Bitbucket Server domain.');
+                return;
+            }
+
+            await removePreviousOptionalPermissionsOrigins();
+
+            const origin = getURLOrigin(bitbucketServerDomainElement.value);
+
+            if (!origin) {
+                setFailedStatus(`'${bitbucketServerDomainElement.value}' is not a valid URL.`);
+                return;
+            }
+
+            chrome.permissions.request({
+                origins: [
+                    `${origin}/*`
+                ]
+            }, granted => {
+                if (granted) {
+                    storeOptions();
+                } else {
+                    setFailedStatus('Couldn\'t grant permissions to authorize a custom Bitbucket Server domain.');
+                }
+            });
+        }
+    }
+
+    function removePreviousOptionalPermissionsOrigins() {
+        return new Promise(async resolve => {
+            const permissions = await new Promise(r => chrome.permissions.getAll(r));
+
+            const promises = permissions.origins.map(o => {
+                return new Promise(r => {
+                    chrome.permissions.remove({ 
+                        origins: [o] 
+                    }, removed => {
+                        if (chrome.runtime.lastError) {
+                            // Ignore remove attempt of required permissions
+                        }
+                        r();
+                    });
+                });
+            });
+
+            await Promise.all(promises);
+            resolve();
+        });
+    }
+
+    function getURLOrigin(url) {
+        try {
+            return new URL(url).origin;
+        } catch (e) {
+        }
+    }
+
+    function storeOptions() {
         const highlightSyntax = highlightSyntaxElement.checked;
         const highlightOcurrences = highlightOcurrencesElement.checked;
         const minimumNumberOfApprovals = approvalsElement.value;
+        const bitbucketServerDomain = bitbucketServerDomainElement.value;
 
         chrome.storage.sync.set({
             highlightSyntax,
             highlightOcurrences,
-            minimumNumberOfApprovals
+            minimumNumberOfApprovals,
+            bitbucketServerDomain
         }, () => {
-            statusElement.textContent = 'Options saved.';
+            setSuccessfulStatus('Options saved.');
             setTimeout(() => {
-                statusElement.textContent = '';
-            }, 750);
+                setSuccessfulStatus('');
+            }, 1500);
         });
     }
 
@@ -29,12 +94,24 @@
         chrome.storage.sync.get({
             highlightSyntax: true,
             highlightOcurrences: true,
-            minimumNumberOfApprovals: 2
+            minimumNumberOfApprovals: 2,
+            bitbucketServerDomain: '',
         }, options => {
             highlightSyntaxElement.checked = options.highlightSyntax;
             highlightOcurrencesElement.checked = options.highlightOcurrences;
             approvalsElement.value = options.minimumNumberOfApprovals;
+            bitbucketServerDomainElement.value = options.bitbucketServerDomain;
         });
+    }
+
+    function setFailedStatus(textContent) {
+        statusElement.style = 'color: red';
+        statusElement.textContent = textContent;
+    }
+
+    function setSuccessfulStatus(textContent) {
+        statusElement.style = 'color: green';
+        statusElement.textContent = textContent;
     }
 
     document.addEventListener('DOMContentLoaded', restoreOptions);
