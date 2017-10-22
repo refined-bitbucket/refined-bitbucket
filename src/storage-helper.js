9e6c0497ef9231d0805db5b890dd571bc463a5e1
Ronald Rey <reyronald@gmail.com>
2017-10-21 23:53:58 -0400

    feat(approvals): fix tests. trailing comma

--- src/storage-helper.js
+++ src/storage-helper.js
@@ -8,11 +8,11 @@ module.exports = (function storageHelper() {
             return new Promise(resolve => {
                 chrome.storage.sync.get({
                     highlightSyntax: true,
-                    highlightOcurrences: true,
+                    highlightOcurrences: true
                 }, options => {
                     resolve({
                         highlightSyntax: options.highlightSyntax,
-                        highlightOcurrences: options.highlightOcurrences,
+                        highlightOcurrences: options.highlightOcurrences
                     });
                 });
             });
