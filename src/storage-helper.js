fd57825e26db291fd2bb0fa3ab74d7612745bae4
Ronald Rey <reyronald@gmail.com>
2017-11-17 10:48:22 -0400

    feat(ignore-whitespace): transform pr link to ignore whitespace by default
    Open pull request links with "Ignore whitespace" option enabled by default when entering pull requests from the pull request list page.
    
    Closes issue #78.

--- src/storage-helper.js
+++ src/storage-helper.js
@@ -9,12 +9,14 @@ module.exports = (function storageHelper() {
                 chrome.storage.sync.get({
                     highlightSyntax: true,
                     highlightOcurrences: true,
+                    ignoreWhitespace: true,
                     autocollapsePaths: ['package-lock.json'],
                     ignorePaths: []
                 }, options => {
                     resolve({
                         highlightSyntax: options.highlightSyntax,
                         highlightOcurrences: options.highlightOcurrences,
+                        ignoreWhitespace: options.ignoreWhitespace,
                         autocollapsePaths: options.autocollapsePaths,
                         ignorePaths: options.ignorePaths
                     });
---
+++ test/ignore-whitespace.spec.js
@@ -0,0 +1,54 @@
+import {h} from 'dom-chef';
+import test from 'ava';
+import {URL} from 'url';
+import URLSearchParams from 'url-search-params';
+
+import './setup-jsdom';
+
+import ignoreWhitespace from '../src/ignore-whitespace';
+
+global.URL = URL;
+global.URLSearchParams = URLSearchParams;
+global.location = new URL('https://www.bitbucket.org');
+
+test('should transform pull request link to add ignore whitespace query param to 1', t => {
+    const actual = <a class="pull-request-title" title="pull request title" href="https://bitbucket.org/user/repo/pull-requests/1">pull request title</a>;
+
+    const expected = <a class="pull-request-title" title="pull request title" href="https://bitbucket.org/user/repo/pull-requests/1?w=1">pull request title</a>;
+
+    document.body.appendChild(actual);
+
+    location.href = 'https://www.bitbucket.org/user/repo/pull-requests/';
+
+    ignoreWhitespace.init();
+
+    t.is(actual.outerHTML, expected.outerHTML);
+});
+
+test('should transform pull request link to toggle ignore whitespace query param to 1', t => {
+    const actual = <a class="pull-request-title" title="pull request title" href="https://bitbucket.org/user/repo/pull-requests/1?w=0">pull request title</a>;
+
+    const expected = <a class="pull-request-title" title="pull request title" href="https://bitbucket.org/user/repo/pull-requests/1?w=1">pull request title</a>;
+
+    document.body.appendChild(actual);
+
+    location.href = 'https://www.bitbucket.org/user/repo/pull-requests/';
+
+    ignoreWhitespace.init();
+
+    t.is(actual.outerHTML, expected.outerHTML);
+});
+
+test('should not transform pull request link if current page is not pull requests list', t => {
+    const actual = <a class="pull-request-title" title="pull request title" href="https://bitbucket.org/user/repo/pull-requests/1">pull request title</a>;
+
+    const expected = <a class="pull-request-title" title="pull request title" href="https://bitbucket.org/user/repo/pull-requests/1">pull request title</a>;
+
+    document.body.appendChild(actual);
+
+    location.href = 'https://www.bitbucket.org/user/repo/pull-requests/is-not-pull-request-page-list'
+
+    ignoreWhitespace.init();
+
+    t.is(actual.outerHTML, expected.outerHTML);
+});
