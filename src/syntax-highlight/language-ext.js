ac198aa6d2d82f874be0203086f3b2c8ced53f83
Ronald Rey <reyronald@gmail.com>
2017-11-17 09:22:24 -0400

    lang(julia): add Julia language support for files with extension `.jl`

--- src/syntax-highlight/language-ext.js
+++ src/syntax-highlight/language-ext.js
@@ -7,6 +7,7 @@ module.exports = {
     '.sh': 'language-bash',
     '.jsp': 'language-java',
     '.java': 'language-java',
+    '.jl': 'language-julia',
     '.kt': 'language-kotlin',
     '.xml': 'language-markup',
     '.html': 'language-markup',
