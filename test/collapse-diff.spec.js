e4b7327972a5703f855d2f4e9b41fef24dab6001
Ronald Rey <reyronald@gmail.com>
2017-12-28 09:59:24 -0400

    finishing touches

--- test/collapse-diff.spec.js
+++ test/collapse-diff.spec.js
@@ -41,6 +41,18 @@ const createNode = () => (
             </div>
         </div>
 
+        <div class="diff-message-container diff-message-container-skipped">
+            <div class="aui-message aui-message-warning warning">
+                <p class="title">
+                <strong>Conflict: File modified in both source and destination</strong>
+                </p>
+
+                To be able to merge you will need to resolve the conflicts manually.
+                <a target="_blank" href="https://confluence.atlassian.com/x/M4H8KQ">Learn how</a>.
+
+            </div>
+        </div>
+
         <div class="diff-content-container refract-container">
             <div class="refract-content-container">
                 <pre>var msg = 'Hello world';</pre>
@@ -49,53 +61,11 @@ const createNode = () => (
     </section>
 );
 
-const isHidden = node => Array.from(node.classList).includes('__refined_bitbucket_hide');
-
 test('should not re-insert collapse diff button if already present', async t => {
-    const uudiff = (
-        <section class="bb-udiff" data-filename="filename.js">
-            <div class="heading">
-                <div class="diff-actions secondary" id="side-by-side-1">
-                    {/* "Side by side" and "View File" buttons */}
-                    <div class="aui-buttons">
-                        <button href="#side-by-side-1-content">
-                            Side-by-side diff
-                        </button>
+    const uudiff = createNode();
 
-                        <a class="view-file ">
-                            View file
-                        </a>
-                    </div>
-
-                    {/* "Comment" button */}
-                    <div class="aui-buttons">
-                        <button class="add-diff-comment add-file-comment">
-                            Comment
-                        </button>
-                    </div>
-
-                    {/* "More" button */}
-                    <div class="aui-buttons">
-                        <button>
-                            <span>More</span>
-                        </button>
-                    </div>
-
-                    {/* "Collapse-diff" button */}
-                    <div class="aui-buttons">
-                        <button class="__refined_bitbucket_collapse_diff_button" aria-label="Toggle diff text" title="Toggle diff text">
-                        </button>
-                    </div>
-
-                </div>
-            </div>
-
-            <div class="refract-content-container">
-                <pre>var msg = 'Hello world';</pre>
-            </div>
-        </section>
-    );
-
+    collapseDiff.insertCollapseDiffButton(uudiff);
+    collapseDiff.insertCollapseDiffButton(uudiff);
     collapseDiff.insertCollapseDiffButton(uudiff);
 
     const buttons = uudiff.getElementsByClassName('__refined_bitbucket_collapse_diff_button');
@@ -110,6 +80,44 @@ test('should insert button in correct position when diff loads successfully', as
     const button = uudiff.querySelector('div.secondary.diff-actions div:nth-child(4)');
 
     t.truthy(button.querySelector('.__refined_bitbucket_collapse_diff_button'));
+});
+
+test('should toggle the diff, toggle messages, toggle the arrow icon and apply bottom border to heading', t => {
+    // Arrange
+    const uudiff = createNode();
+
+    // Acting
+    collapseDiff.insertCollapseDiffButton(uudiff);
+
+    const button = uudiff.querySelector('.__refined_bitbucket_collapse_diff_button');
+    const diffContentContainer = uudiff.querySelector('div.diff-content-container');
+    const diffMessageContainer = uudiff.querySelector('div.diff-message-container');
+    const heading = uudiff.querySelector('div.heading');
+    const upArrow = button.querySelector('svg[data-arrow-direction="up"]');
+    const downArrow = button.querySelector('svg[data-arrow-direction="down"]');
+
+    button.click();
+
+    // Assering
+    const hasClass = (node, className) => Array.from(node.classList).includes(className);
+    const isHidden = node => hasClass(node, '__refined_bitbucket_hide');
+    const hasBorder = node => hasClass(node, '__refined_bitbucket_bottom_border');
+
+    t.true(isHidden(diffContentContainer));
+    t.true(isHidden(diffMessageContainer));
+    t.true(isHidden(upArrow));
+    t.true(!isHidden(downArrow));
+    t.true(hasBorder(heading));
+
+    // Acting
+    button.click();
+
+    // Assering
+    t.false(isHidden(diffContentContainer));
+    t.false(isHidden(diffMessageContainer));
+    t.false(isHidden(upArrow));
+    t.false(!isHidden(downArrow));
+    t.false(hasBorder(heading));
 });
 
 test('should insert button in correct position if diff failed to load', async t => {
@@ -142,51 +150,4 @@ test('should insert button in correct position if diff failed to load', async t 
 
     const button = uudiff.querySelector('div.secondary.diff-actions .__refined_bitbucket_collapse_diff_button');
     t.truthy(button);
-});
-
-test('should toggle the diff, toggle the arrow icon and apply bottom border to heading', t => {
-    // Arrange
-    const uudiff = createNode();
-
-    // Acting
-    collapseDiff.insertCollapseDiffButton(uudiff);
-
-    const button = uudiff.querySelector('.__refined_bitbucket_collapse_diff_button');
-    const diffContentContainer = uudiff.querySelector('div.diff-content-container');
-    const heading = uudiff.querySelector('div.heading');
-    const upArrow = button.querySelector('svg[data-arrow-direction="up"]');
-    const downArrow = button.querySelector('svg[data-arrow-direction="down"]');
-
-    button.click();
-
-    // Assering
-    t.true(isHidden(diffContentContainer));
-    t.true(isHidden(upArrow));
-    t.true(!isHidden(downArrow));
-    t.true(Array.from(heading.classList).includes('__refined_bitbucket_bottom_border'));
-
-    // Acting
-    button.click();
-
-    // Assering
-    t.false(isHidden(diffContentContainer));
-    t.false(isHidden(upArrow));
-    t.false(!isHidden(downArrow));
-    t.false(Array.from(heading.classList).includes('__refined_bitbucket_bottom_border'));
-});
-
-test.failing('should hide the diff when it\'s only file rename', t => {
-    t.fail();
-});
-
-test.failing('should hide the diff when file contents are unchanged (only whitespaces)', t => {
-    t.fail();
-});
-
-test.failing('should hide diff messages', t => {
-    t.fail();
-});
-
-test.failing('should change button svg icon when clicked', t => {
-    t.fail();
 });
