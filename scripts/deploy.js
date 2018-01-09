2bf7071e5fadad6844c2f45664d6608f29525e20
Ronald Rey <reyronald@gmail.com>
2018-01-09 08:51:49 -0400

    firefox deploy

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -3,18 +3,34 @@ require('dotenv').config();
 const fs = require('fs');
 const path = require('path');
 const chromeDeploy = require('chrome-extension-deploy');
+const firefoxDeploy = require('firefox-extension-deploy');
 
 console.log('Deploying to Chrome Web Store...');
+
+const zipPath = path.join(__dirname, '../extension.zip');
 
 chromeDeploy({
     clientId: process.env.CHROME_CLIENT_ID,
     clientSecret: process.env.CHROME_CLIENT_SECRET,
     refreshToken: process.env.CHROME_REFRESH_TOKEN,
     id: 'afppminkfnfngihdocacbgeajbbdklkf',
-    zip: fs.readFileSync(path.join(__dirname, '../extension.zip')),
+    zip: fs.readFileSync(zipPath),
 }).then(() => {
     console.log('Chrome deployment complete!');
 }, err => {
     console.error('Chrome deployment failed:', err);
+    process.exitCode = 1;
+});
+
+firefoxDeploy({
+    issuer: process.env.FIREFOX_ISSUER,
+    secret: process.env.FIREFOX_SECRET,
+    id: 'refined-bitbucket@refined-bitbucket.org',
+    version: require('../extension/manifest.json').version, // eslint-disable-line global-require
+    src: fs.createReadStream(zipPath),
+}).then(() => {
+    console.log('Firefox beta deployment complete!');
+}, err => {
+    console.error('Firefox beta failed:', err);
     process.exitCode = 1;
 });
--- scripts/package.js
+++ scripts/package.js
@@ -1,4 +1,8 @@
+const del = require('del');
 const AdmZip = require('adm-zip');
+
+// Delete source maps
+del.sync(['extension/*.js.map']);
 
 // Archive the extension folder into 'extension.zip'
 const zip = new AdmZip();
