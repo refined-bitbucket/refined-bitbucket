7a881e9d08c5fc6cdd8635d8de70e81d49903542
Ronald Rey <reyronald@gmail.com>
2018-03-06 20:53:44 -0400

    fix(options): allow disable update notifications
    Now you can disable update notifications through the Options page.
    
    Closes #167

--- src/background.js
+++ src/background.js
@@ -2,62 +2,45 @@
 
 import OptionsSync from 'webext-options-sync';
 
-chrome.runtime.onInstalled.addListener(details => {
-    if (details.reason === 'install' || details.reason === 'update') {
-        window.open(
-            'https://github.com/refined-bitbucket/refined-bitbucket/releases/latest'
-        );
-    }
+const justInstalledOrUpdated = new Promise((resolve, reject) => {
+    chrome.runtime.onInstalled.addListener(details => {
+        if (details.reason === 'install' || details.reason === 'update') {
+            resolve();
+        } else {
+            reject();
+        }
+    });
 });
 
-/**
- *  Needed to migrate any saved settings from v3.3.0
- *  and below to v3.4.0 and above
- */
-// null to get the entire storage
-chrome.storage.sync.get(null, deprecatedOptions => {
-    new OptionsSync().define({
-        defaults: {
-            syntaxHighlight: true,
-            highlightOcurrences: true,
-            ignoreWhitespace: true,
-            keymap: true,
-            collapseDiff: true,
-            loadAllDiffs: true,
-            closeAnchorBranch: true,
-            improveFonts: true,
-            addSidebarCounters: true,
-            diffPlusesAndMinuses: true,
-            augmentPrEntry: true,
-            prTemplateEnabled: true,
-            defaultMergeStrategy: 'merge_commit',
-            autocollapsePaths: ['package-lock.json', 'yarn.lock'],
-            autocollapseDeletedFiles: true,
-            ignorePaths: ['']
+new OptionsSync().define({
+    defaults: {
+        syntaxHighlight: true,
+        highlightOcurrences: true,
+        ignoreWhitespace: true,
+        keymap: true,
+        collapseDiff: true,
+        loadAllDiffs: true,
+        closeAnchorBranch: true,
+        improveFonts: true,
+        addSidebarCounters: true,
+        diffPlusesAndMinuses: true,
+        augmentPrEntry: true,
+        prTemplateEnabled: true,
+        defaultMergeStrategy: 'merge_commit',
+        autocollapsePaths: ['package-lock.json', 'yarn.lock'],
+        autocollapseDeletedFiles: true,
+        ignorePaths: [''],
+        eneableUpdateNotifications: true
+    },
+    migrations: [
+        async savedOptions => {
+            if (savedOptions.eneableUpdateNotifications) {
+                await justInstalledOrUpdated;
+                window.open(
+                    'https://github.com/refined-bitbucket/refined-bitbucket/releases/latest'
+                );
+            }
         },
-        migrations: [
-            (newOptions, currentDefaults) => {
-                Object.keys(currentDefaults).forEach(key => {
-                    if (key in deprecatedOptions) {
-                        newOptions[key] = deprecatedOptions[key];
-                    }
-                });
-
-                // Until v3.3.0, these paths were saved as arrays.
-                // Since we are now using 'webext-options-sync',
-                // every stored value must be a primitive
-                if (Array.isArray(newOptions.ignorePaths)) {
-                    newOptions.ignorePaths = newOptions.ignorePaths.join('\n');
-                }
-                if (Array.isArray(newOptions.autocollapsePaths)) {
-                    newOptions.autocollapsePaths = newOptions.autocollapsePaths.join(
-                        '\n'
-                    );
-                }
-
-                chrome.storage.sync.clear();
-            },
-            OptionsSync.migrations.removeUnused
-        ]
-    });
+        OptionsSync.migrations.removeUnused
+    ]
 });
