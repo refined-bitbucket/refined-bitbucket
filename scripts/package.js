6010d50b7fbe8a211ebc2652e10a2bb97cd1eb20
Ronald Rey <reyronald@gmail.com>
2018-04-02 19:42:26 -0400

    fix: `package` script produces valid zip
    Fix the `package` npm script to use the `archiver` npm package instead
    of `admin-zip` so that it produces a valid Zip file.
    
    Closes #193
    
    https://github.com/mozilla/addons/issues/672
    https://github.com/mozilla/addons-linter/issues/1938

--- scripts/package.js
+++ scripts/package.js
@@ -1,10 +1,23 @@
+const fs = require('fs');
 const del = require('del');
-const AdmZip = require('adm-zip');
+const archiver = require('archiver');
 
 // Delete source maps
 del.sync(['extension/*.js.map']);
 
 // Archive the extension folder into 'extension.zip'
-const zip = new AdmZip();
-zip.addLocalFolder('extension');
-zip.writeZip('extension.zip');
+const output = fs.createWriteStream('extension.zip');
+const archive = archiver('zip');
+
+output.on('close', () => {
+    const kbytes = Math.round(archive.pointer() / 1024 * 100) / 100;
+    console.log(`'extension.zip' (${kbytes} KB) file generated.`);
+});
+
+archive.on('error', err => {
+    throw err;
+});
+
+archive.pipe(output);
+archive.directory('extension', false);
+archive.finalize();
