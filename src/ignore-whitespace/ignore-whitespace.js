dd110c1f109dbec72f7abd30e6ed30dc5e54cda2
Ronald Rey <reyronald@gmail.com>
2018-09-06 16:44:06 -0400

    Flow `ignore-whitespace`

--- src/ignore-whitespace/ignore-whitespace.js
+++ src/ignore-whitespace/ignore-whitespace.js
@@ -1,8 +1,12 @@
-export default function init(prRow) {
-    const link = prRow.querySelector('a.pull-request-title')
+// @flow
+
+export default function init(prRow: Element) {
+    const link: HTMLAnchorElement = (prRow.querySelector(
+        'a.pull-request-title'
+    ): any)
     const url = new URL(link.href)
     const searchParams = new URLSearchParams(url.search)
-    searchParams.set('w', 1)
+    searchParams.set('w', '1')
     url.search = searchParams.toString()
     link.href = url.href
 }
