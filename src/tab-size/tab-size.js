cbddc2db41dfa3e823943100da77598b23f3a4bc
Ronald Rey <reyronald@gmail.com>
2018-09-09 21:10:26 -0400

    Flow `tab-size`

--- src/tab-size/tab-size.js
+++ src/tab-size/tab-size.js
@@ -1,6 +1,7 @@
+// @flow
 import addStyleToPage from '../add-style'
 
-export default function setTabSize(numSpaces) {
+export default function setTabSize(numSpaces: number | string) {
     const cssRule = createCssRule(numSpaces)
     addStyleToPage(cssRule)
 }
