c39dc7efd6787bfcc589b4ea71640b22778a3ef3
Ronald Rey <reyronald@gmail.com>
2019-02-10 12:47:25 -0400

    Add tests

--- src/collapse-pull-request-description/collapse-pull-request-description.spec.js
+++ src/collapse-pull-request-description/collapse-pull-request-description.spec.js
@@ -1,8 +1,93 @@
 import test from 'ava'
 import { h } from 'dom-chef'
 
+import '../../test/setup-jsdom'
 import collapsePullRequestDescription from '.'
 
-test('should toggle pull request description properly', () => {
-    //
+test('should toggle pull request description properly', t => {
+    // Arrange
+    const actual = (
+        <div class="clearfix description">
+            <dt>Description</dt>
+            <dd class="wiki-content">Description content</dd>
+        </div>
+    )
+
+    const expected = (
+        <div class="clearfix description">
+            <dt>Description</dt>
+            <dd>
+                <button
+                    type="button"
+                    aria-label="Toggle description text"
+                    title="Toggle description text"
+                    class="__rbb-collapse-bar"
+                >
+                    <svg
+                        aria-hidden="true"
+                        height="16"
+                        version="1.1"
+                        viewBox="0 0 10 16"
+                        width="10"
+                        data-arrow-direction="up"
+                    >
+                        <path
+                            fill-rule="evenodd"
+                            d="M10 10l-1.5 1.5L5 7.75 1.5 11.5 0 10l5-5z"
+                        />
+                    </svg>
+                    <svg
+                        aria-hidden="true"
+                        height="16"
+                        version="1.1"
+                        viewBox="0 0 10 16"
+                        width="10"
+                        class="__refined_bitbucket_hide"
+                        data-arrow-direction="down"
+                    >
+                        <path
+                            fill-rule="evenodd"
+                            d="M5 11L0 6l1.5-1.5L5 8.25 8.5 4.5 10 6z"
+                        />
+                    </svg>
+                </button>
+            </dd>
+            <dd class="wiki-content">Description content</dd>
+        </div>
+    )
+
+    document.body.appendChild(actual)
+
+    // Act
+    collapsePullRequestDescription()
+
+    // Assert
+    t.is(actual.outerHTML, expected.outerHTML)
+
+    const button = actual.querySelector('button')
+    const upArrow = actual.querySelector('svg[data-arrow-direction="up"]')
+    const downArrow = actual.querySelector('svg[data-arrow-direction="down"]')
+    const descriptionContent = actual.querySelector('.wiki-content')
+    const isHidden = el =>
+        [...el.classList].includes('__refined_bitbucket_hide')
+
+    t.false(isHidden(upArrow))
+    t.true(isHidden(downArrow))
+    t.false(isHidden(descriptionContent))
+
+    // Act
+    button.click()
+
+    // Assert
+    t.true(isHidden(upArrow))
+    t.false(isHidden(downArrow))
+    t.true(isHidden(descriptionContent))
+
+    // Act
+    button.click()
+
+    // Assert
+    t.false(isHidden(upArrow))
+    t.true(isHidden(downArrow))
+    t.false(isHidden(descriptionContent))
 })
