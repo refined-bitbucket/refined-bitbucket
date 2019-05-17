4898e8957653416f0a7ad3aabe813ec65263f4cb
Luke Denton <denno020@gmail.com>
2019-05-17 18:56:42 +0930

    Create sticky header feature file
    Includes the CSS to set the sticky position for both large screens and Bitbucket's "mobile" breakpoint, where a toolbar is displayed at the top of the page

---
+++ src/sticky-header/sticky-header.js
@@ -0,0 +1,25 @@
+// @flow
+
+import addStyleToPage from '../add-style'
+
+export default function setStickyHeader() {
+    const cssRule = createCssRule()
+    addStyleToPage(cssRule)
+}
+
+function createCssRule() {
+    const cssRule = `
+        .diff-container .heading {
+            position: sticky;
+            top: 0px;
+            z-index: 9;
+        }
+
+        @media (max-width: 979px) {
+            .diff-container .heading {
+                top: 54px;
+            }
+        }
+    `
+    return cssRule
+}
