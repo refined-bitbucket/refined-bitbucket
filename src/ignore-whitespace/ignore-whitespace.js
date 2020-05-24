8d27ebc576f491e58659dfe0d5151b723ca11cc0
jwallet <joseouellet@gmail.com>
2020-05-23 21:31:42 -0400

    fix build

--- src/ignore-whitespace/ignore-whitespace.js
+++ src/ignore-whitespace/ignore-whitespace.js
@@ -17,7 +17,7 @@ export async function ignoreWhitespaceInit() {
         '[data-key="ignoreWhitespace"]'
     )
     if (toggle && toggle.getAttribute('aria-checked') === 'false') {
-        toggle.dataset['jumpTo'] = '#'
+        toggle.dataset.jumpTo = '#'
         toggle.click()
     }
 }
