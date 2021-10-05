b138957e205f343ece75e70be2620e1d905dfa28
Bert Verhelst <verhelstbert@gmail.com>
2021-10-05 11:15:48 +0200

    fix(total-lines-changed): fix build issues with nesting depth

--- src/total-lines-changed/total-lines-changed.js
+++ src/total-lines-changed/total-lines-changed.js
@@ -14,8 +14,53 @@ function toReadableNumber(num: number): string {
         return String(Math.round(num / 1000000)) + 'M'
     } else if (num > 1000) {
         return String(Math.round(num / 1000)) + 'K'
+    }
+    return String(num)
+}
+
+function handleDiffStats(diffStats) {
+    if (diffStats && diffStats.size) {
+        if (diffStats.size < 1000) {
+            const totalAdded: number = diffStats.values
+                .map(val => val.lines_added)
+                .reduce((partialSum, a) => partialSum + a, 0)
+            const totalRemoved: number = diffStats.values
+                .map(val => val.lines_removed)
+                .reduce((partialSum, a) => partialSum + a, 0)
+
+            const linesAddedBadge = (
+                <span class="__rbb-total-lines-added">
+                    +{toReadableNumber(totalAdded)}
+                </span>
+            )
+            const linesRemovedBadge = (
+                <span class="__rbb-total-lines-removed">
+                    -{toReadableNumber(totalRemoved)}
+                </span>
+            )
+            // Refetch since the element could have been re-rendered in the meantime
+            const filesTab: HTMLElement | nullfilesTab = document.querySelector(
+                FILES_TAB_SELECTOR
+            )
+            const filesCounter = document.querySelector(
+                `${FILES_TAB_SELECTOR} > span:nth-child(2)`
+            )
+            if (filesTab && filesCounter) {
+                filesTab.insertBefore(
+                    linesRemovedBadge,
+                    filesCounter.nextSibling
+                )
+                filesTab.insertBefore(linesAddedBadge, filesCounter.nextSibling)
+            } else {
+                console.warn('Failed to find files tab counter')
+            }
+        } else {
+            console.warn(
+                'More than a 1000 files changed, skipping total calculation'
+            )
+        }
     } else {
-        return String(num)
+        console.warn('There seem to be no changed files')
     }
 }
 
@@ -46,51 +91,7 @@ export default async function totalLinesChanged(url: string) {
 
         if (hash1 && hash2) {
             const diffStats = await api.getPullrequestFiles(prId, hash1, hash2)
-
-            if (diffStats && diffStats.size) {
-                if (diffStats.size < 1000) {
-                    const totalAdded: number = diffStats.values
-                        .map(val => val.lines_added)
-                        .reduce((partialSum, a) => partialSum + a, 0)
-                    const totalRemoved: number = diffStats.values
-                        .map(val => val.lines_removed)
-                        .reduce((partialSum, a) => partialSum + a, 0)
-
-                    const linesAddedBadge = (
-                        <span class="__rbb-total-lines-added">
-                            +{toReadableNumber(totalAdded)}
-                        </span>
-                    )
-                    const linesRemovedBadge = (
-                        <span class="__rbb-total-lines-removed">
-                            -{toReadableNumber(totalRemoved)}
-                        </span>
-                    )
-                    // Refetch since the element could have been re-rendered in the meantime
-                    filesTab = document.querySelector(FILES_TAB_SELECTOR)
-                    const filesCounter = document.querySelector(
-                        `${FILES_TAB_SELECTOR} > span:nth-child(2)`
-                    )
-                    if (filesTab && filesCounter) {
-                        filesTab.insertBefore(
-                            linesRemovedBadge,
-                            filesCounter.nextSibling
-                        )
-                        filesTab.insertBefore(
-                            linesAddedBadge,
-                            filesCounter.nextSibling
-                        )
-                    } else {
-                        console.warn('Failed to find files tab counter')
-                    }
-                } else {
-                    console.warn(
-                        'More than a 1000 files changed, skipping total calculation'
-                    )
-                }
-            } else {
-                console.warn('There seem to be no changed files')
-            }
+            handleDiffStats(diffStats)
         } else {
             console.warn('Could not find hashes to fetch file diffs')
         }
@@ -98,5 +99,5 @@ export default async function totalLinesChanged(url: string) {
         console.warn('Could not find pull request id')
     }
 
-    return filesTab
+    return document.querySelector(FILES_TAB_SELECTOR)
 }
